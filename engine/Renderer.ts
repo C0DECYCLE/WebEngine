99ac55f4edfaa446d3477344d924a8c5eb94ffc2
Noah Bussinger <development@palto.ch>
2023-02-19 10:20:58 +0100

    Revert webgpu

--- engine/Renderer.ts
+++ engine/Renderer.ts
@@ -5,43 +5,15 @@
 */
 
 class Renderer {
-
     private canvas: HTMLCanvasElement;
-    private adapter: GPUAdapter;
-    private device: GPUDevice;
-    private context: GPUCanvasContext;
+    private context: WebGL2RenderingContext;
 
     public constructor() {
-
         this.createCanvas();
+        this.initialize();
     }
-
-    public async initialize() {
-
-        const adapter: Nullable<GPUAdapter> = await navigator.gpu?.requestAdapter();
-
-        if (!adapter) {
-            console.error("Renderer: GPUAdapter request failed.");
-            return;
-        }
-
-        this.adapter = adapter;
-        this.device = await this.adapter.requestDevice();
-
-        const context: Nullable<GPUCanvasContext> = this.canvas.getContext("webgpu");
-
-        if (!context) {
-            console.error("Renderer: Get GPUCanvasContext failed.");
-            return;
-        }
-
-        this.context = context;
-        this.configureContext();
-    }
-
 
     private createCanvas() {
-
         this.canvas = document.createElement("canvas");
         this.canvas.style.width = "100%";
         this.canvas.style.height = "100%";
@@ -50,20 +22,18 @@ class Renderer {
         document.body.appendChild(this.canvas);
     }
 
-    private configureContext() {
+    private initialize() {
+        const context: Nullable<WebGL2RenderingContext> =
+            this.canvas.getContext("webgl2");
 
-        const devicePixelRatio: int = window.devicePixelRatio || 1;
-        const presentationSize: [int, int] = [
-            this.canvas.clientWidth * devicePixelRatio,
-            this.canvas.clientHeight * devicePixelRatio
-        ];
-        const presentationFormat: GPUTextureFormat = navigator.gpu.getPreferredCanvasFormat();
+        if (!context) {
+            console.error("Renderer: Get WebGL2RenderingContext failed.");
+            return;
+        }
 
-        this.context.configure({
-            device: this.device,
-            size: presentationSize,
-            format: presentationFormat,
-            alphaMode: 'opaque'
-        });
+        this.context = context;
+        this.configureContext();
     }
+
+    private configureContext() {}
 }
