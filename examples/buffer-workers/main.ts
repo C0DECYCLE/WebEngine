95b11668fbeb036883a946e970af0d3b9a1e7894
Noah Bussinger <development@palto.ch>
2023-02-20 19:31:49 +0100

    Test sharedarraybuffer

--- examples/buffer-workers/main.ts
+++ examples/buffer-workers/main.ts
@@ -7,21 +7,31 @@
 window.addEventListener("compile", (_event: Event): void => {
     const n: int = 100_000;
 
-    const camPos: Float64Array = new Float64Array(3);
+    const camPosBuffer: SharedArrayBuffer = new SharedArrayBuffer(
+        Float64Array.BYTES_PER_ELEMENT * 3
+    );
+    const camPos: Float64Array = new Float64Array(camPosBuffer);
     camPos[0] = 1_000_000;
     camPos[1] = 10;
     camPos[2] = 100_000;
 
     //log(n, camPos);
 
-    const positions: Float64Array = new Float64Array(3 * n);
-    const matricies: Float32Array = new Float32Array(16 * n);
+    const positionsBuffer: SharedArrayBuffer = new SharedArrayBuffer(
+        Float64Array.BYTES_PER_ELEMENT * 3 * n
+    );
+    const positions: Float64Array = new Float64Array(positionsBuffer);
 
     for (let i: int = 0; i < n; i++) {
         positions[i * 3 + 0] = camPos[0] + (Math.random() * 2 - 1) * 100_000;
         positions[i * 3 + 2] = camPos[2] + (Math.random() * 2 - 1) * 100_000;
         positions[i * 3 + 1] = camPos[1] + (Math.random() * 2 - 1) * 100_000;
     }
+
+    const matriciesBuffer: SharedArrayBuffer = new SharedArrayBuffer(
+        Float64Array.BYTES_PER_ELEMENT * 16 * n
+    );
+    const matricies: Float32Array = new Float32Array(matriciesBuffer);
 
     for (let j: int = 0; j < n; j++) {
         matricies[j * 16 + 0] = 1;
@@ -47,7 +57,7 @@ window.addEventListener("compile", (_event: Event): void => {
 
     //log(positions, matricies);
 
-    let s: int = 1_000;
+    let s: int = 0; //1_000;
     let mss: float = 0;
 
     for (let t: int = 0; t < s; t++) {
