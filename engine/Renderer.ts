f48c52678b9606a103a736420de3f86c3feb1af3
Noah Bussinger <development@palto.ch>
2023-02-27 14:08:00 +0100

    Update comment

--- engine/Renderer.ts
+++ engine/Renderer.ts
@@ -27,13 +27,12 @@ class Renderer {
 
         //////////////////SETUP//////////////////
 
-        //matrix
-        const matrix: Mat4 = this.camera.projection.clone();
+        const matrix: Mat4 = this.camera.projection.clone(); //object world transform matrix
         matrix.translate(0, 125, -300);
         matrix.rotate(135 * toRadian, 0 * toRadian, 22.5 * toRadian);
         matrix.scale(1.5, 1.5, 1.5);
-        //vertecies
-        const stride: int = 3;
+
+        const stride: int = 3; //attribute stride
         const positions: Float32Array = new Float32Array([
             0, 0, 0, 0, 150, 0, 30, 0, 0, 0, 150, 0, 30, 150, 0, 30, 0, 0, 30,
             0, 0, 30, 30, 0, 100, 0, 0, 30, 30, 0, 100, 30, 0, 100, 0, 0, 30,
@@ -52,35 +51,35 @@ class Renderer {
             0, 30, 150, 30, 0, 150, 0, 0, 150, 30, 30, 150, 30, 0, 150, 0, 30,
             150, 30, 30, 150, 0, 0, 0, 0, 0, 0, 30, 0, 150, 30, 0, 0, 0, 0, 150,
             30, 0, 150, 0,
-        ]);
+        ]); //vertecies cpu buffer
 
-        //make shaders
-        const program: WebGLProgram = this.shaderManager.programs.get("basic")!;
+        const program: WebGLProgram = this.shaderManager.programs.get("basic")!; //make shaders and shader programm
 
-        //lookup uniform location
         const matrixUniformLocation: Nullable<WebGLUniformLocation> =
-            this.gl.getUniformLocation(program, "objectMatrix");
-        //lookup attribute location
+            this.gl.getUniformLocation(program, "objectMatrix"); //lookup uniform location
+
         const positionAttributeLocation: int = this.gl.getAttribLocation(
             program,
             "vertexPosition"
-        );
+        ); //lookup attribute location
 
-        //create vertex array object
         const vao: Nullable<WebGLVertexArrayObject> =
-            this.gl.createVertexArray();
-        //enable putting stuff into vao
-        this.gl.bindVertexArray(vao);
-        //tell how to map the vertecies and the program
-        this.gl.enableVertexAttribArray(positionAttributeLocation);
-        //put vertecies into gpu buffer
-        const positionBuffer: Nullable<WebGLBuffer> = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+            this.gl.createVertexArray(); //create vertex array object
+
+        this.gl.bindVertexArray(vao); //enable putting stuff into vao
+
+        this.gl.enableVertexAttribArray(positionAttributeLocation); //enable data attribute
+
+        const positionBuffer: Nullable<WebGLBuffer> = this.gl.createBuffer(); //create gpu buffer
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer); //set transfer target
+
         this.gl.bufferData(
             this.gl.ARRAY_BUFFER,
             positions,
             this.gl.STATIC_DRAW
-        );
+        ); //transfer cpu to gpu buffer
+
         this.gl.vertexAttribPointer(
             positionAttributeLocation,
             stride,
@@ -88,22 +87,21 @@ class Renderer {
             false,
             0,
             0
-        );
-        //finish with setting up this vao
-        this.gl.bindVertexArray(null);
+        ); //tell how to pull data out of buffer
+
+        this.gl.bindVertexArray(null); //finish with setting up this vao
 
         //////////////////LOOP//////////////////
 
         this.clearContext();
 
-        //set current program
-        this.gl.useProgram(program);
-        //set vertex array object to use
-        this.gl.bindVertexArray(vao);
-        //set unifrom for object
-        this.gl.uniformMatrix4fv(matrixUniformLocation, false, matrix.values);
-        //draw call
-        this.gl.drawArrays(this.gl.TRIANGLES, 0, positions.length / stride);
+        this.gl.useProgram(program); //set current program
+
+        this.gl.bindVertexArray(vao); //set vertex array object to use
+
+        this.gl.uniformMatrix4fv(matrixUniformLocation, false, matrix.values); //set unifrom for object
+
+        this.gl.drawArrays(this.gl.TRIANGLES, 0, positions.length / stride); //draw call
     }
 
     private createCanvas(): HTMLCanvasElement {
