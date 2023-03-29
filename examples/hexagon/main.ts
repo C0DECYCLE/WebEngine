8004c1be283863a3e16793af4e70d13fd1cc7892
Noah Bussinger <development@palto.ch>
2023-03-30 00:05:41 +0200

    Scroll and zoom example

--- examples/hexagon/main.ts
+++ examples/hexagon/main.ts
@@ -7,7 +7,7 @@
 var renderer: Renderer;
 
 window.addEventListener("compile", async (_event: Event): Promise<void> => {
-    renderer = new Renderer(new Vec3(0.9, 0.9, 0.9), undefined, true);
+    renderer = new Renderer(new Vec3(0.9, 0.9, 0.9), undefined, false);
     await renderer.initialize(
         [
             "models/tree.obj",
@@ -21,7 +21,33 @@ window.addEventListener("compile", async (_event: Event): Promise<void> => {
 
     const camera: Camera = renderer.getCamera();
     camera.target.set(-0.1, 0, -0.25).scale(80);
-    camera.position.set(0, 1.25, -1.0).scale(50).add(camera.target);
+
+    const zoomSpeed: float = 0.025;
+    let zoom: float = 40;
+    addEventListener("wheel", (event: WheelEvent) => {
+        zoom += event.deltaY * zoomSpeed;
+        zoom = zoom.clamp(20, 60);
+        camera.position.set(0, 1.25, -1.0).scale(zoom).add(camera.target);
+    });
+    camera.position.set(0, 1.25, -1.0).scale(zoom).add(camera.target);
+
+    const dragSpeed: float = 0.15;
+    let dragging: boolean = false;
+    addEventListener("mousedown", (_event: MouseEvent) => (dragging = true));
+    addEventListener("mouseup", (_event: MouseEvent) => (dragging = false));
+    addEventListener("mousemove", (event: MouseEvent) => {
+        if (!dragging) {
+            return;
+        }
+        camera.target.add(
+            event.movementX * dragSpeed * zoom * 0.015,
+            0.0,
+            event.movementY * dragSpeed * zoom * 0.015
+        );
+        camera.target.x = camera.target.x.clamp(-80, 140);
+        camera.target.z = camera.target.z.clamp(-140, 100);
+        camera.position.set(0, 1.25, -1.0).scale(zoom).add(camera.target);
+    });
 
     const light: Light = renderer.getLight();
     light.ambient.set(0.15, 0.05, 0.2);
@@ -57,7 +83,6 @@ window.addEventListener("compile", async (_event: Event): Promise<void> => {
             field.position.z += map.length * 0.5 * 17.4;
             if (Math.random() > 0.5) field.rotation.y = 180 * toRadian;
             field.attach(renderer);
-            //field.staticLod(0);
             field.shadow(true, true);
             field.wakeUp();
 
@@ -70,7 +95,6 @@ window.addEventListener("compile", async (_event: Event): Promise<void> => {
                     tree.position.z += (Math.random() * 2 - 1) * 8;
                     tree.rotation.y = Math.random() * 360 * toRadian;
                     tree.attach(renderer);
-                    //tree.staticLod(0);
                     tree.shadow(true, true);
                     tree.wakeUp();
                 }
@@ -83,17 +107,16 @@ window.addEventListener("compile", async (_event: Event): Promise<void> => {
             house.position.z += (Math.random() * 2 - 1) * 3;
             house.rotation.y = Math.random() * 360 * toRadian;
             house.attach(renderer);
-            //house.staticLod(0);
             house.shadow(true, true);
             house.wakeUp();
         }
     }
 
     for (let z: int = 0; z < 8; z++) {
-        for (let x: int = 0; x < 8; x++) {
+        for (let x: int = 0; x < 10; x++) {
             const water: Entity = new Entity("water");
             water.position.set(-x * 60, -2.0, -z * 60);
-            water.position.x += 4 * 60;
+            water.position.x += 5 * 60;
             water.position.z += 4 * 60;
             if (x % 2 == 0) water.rotation.y = 180 * toRadian;
             water.attach(renderer);
@@ -104,29 +127,21 @@ window.addEventListener("compile", async (_event: Event): Promise<void> => {
 
             const sand: Entity = new Entity("sand");
             sand.position.set(-x * 60, 0.0, -z * 60);
-            sand.position.x += 4 * 60;
+            sand.position.x += 5 * 60;
             sand.position.z += 4 * 60;
             sand.attach(renderer);
             sand.setViewCulling(-0.15);
-            //sand.staticLod(0);
             sand.shadow(false, false);
             sand.wakeUp();
         }
     }
 
     function render(now: float): void {
-        /*
-        camera.position
-            .set(0, 1.25, -1)
-            .scale(now * 0.005)
-            .add(camera.target);
-        //shadow.position.copy(camera.position);
-        */
-        /*
-        light.direction
-            .set(-Math.cos(2 + now * 0.0005), -Math.sin(2 + now * 0.0005), 0.5)
-            .normalize();
-        */
+        shadow.position.copy(camera.target).add(0.0, 0.0, zoom);
+        shadow.position.x = Math.round(shadow.position.x / 10) * 10;
+        shadow.position.y = Math.round(shadow.position.y / 10) * 10;
+        shadow.position.z = Math.round(shadow.position.z / 10) * 10;
+        shadow.radius = Math.round((zoom * 2.5) / 10) * 10;
 
         renderer.render(now);
 
